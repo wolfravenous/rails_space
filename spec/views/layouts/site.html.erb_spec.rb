694c263857d5dceecafda748c5a6692b40e6c95c
Steven Hampton <wolfravenous@gmail.com>
2009-01-31 01:18:28 -0500

    created layout site view and removed public/index file
    created layout site view and a simple spec file to go with it
    and removed the public/index file

---
+++ spec/views/layouts/site.html.erb_spec.rb
@@ -0,0 +1,17 @@
+require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
+
+describe "layouts/site" do
+  before(:each) do
+    render 'layouts/site'
+  end
+  
+  #Delete this example and add some real ones or delete this file
+  it "should tell you the title" do
+    response.should have_tag('title')
+  end  
+  
+  it "should have a body" do
+      response.should have_tag('body')
+    
+  end
+end
