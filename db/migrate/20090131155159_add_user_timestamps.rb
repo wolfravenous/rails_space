ebafeca8d6d31851058a749f3cc8ba0e9901ef63
Steven Hampton <wolfravenous@gmail.com>
2010-12-11 15:28:02 -0500

    removed some older rails commands to create timestamps for user accounts

--- db/migrate/20090131155159_add_user_timestamps.rb
+++ db/migrate/20090131155159_add_user_timestamps.rb
@@ -1,11 +1,8 @@
 class AddUserTimestamps < ActiveRecord::Migration
   def self.up
-    add_column :users, :created_at, :timestamp 
-    add_column :users, :updated_at, :timestamp
+    #This is handled by t.timestamps in the current version of rails.
   end
 
   def self.down
-    remove_column :users, :created_at 
-    remove_column :users, :updated_at
   end
 end
