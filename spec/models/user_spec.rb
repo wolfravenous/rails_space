b3cefca229d76814faefd012306669a620668175
Steven Hampton <wolfravenous@gmail.com>
2010-12-11 15:29:27 -0500

    added the user specifications to the spec model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,6 +3,9 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 describe User do
   before(:each) do
     @valid_attributes = {
+      :screen_name =>  "MyString",
+      :email =>  "mystring@example.com",
+      :password =>  "mypassword"
     }
   end
 
